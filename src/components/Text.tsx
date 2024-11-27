11407db109f22d38a156586b5c4a563b1979ce36
Monalisa de Moraes <monalisamoraeszm@gmail.com>
2024-11-27 16:01:55 -0300

    first commit

---
+++ src/components/Text.tsx
@@ -0,0 +1,26 @@
+import {FC} from "react";
+
+enum FontSizes {
+  "s" = "text-sm",
+  "m" = "text-base",
+  "l" = "text-lg"
+}
+interface TextProps {
+  size?: keyof typeof FontSizes;
+  span?: boolean;
+  bold?: boolean;
+}
+export const Text: FC<TextProps> = ({ size = "m", span, bold, children }) => {
+  const Tag = span ? "span" : "p";
+
+  return (
+    <Tag
+      className={`self-stretch text-gray-950 dark:text-gray-50 ${
+        FontSizes[size]
+      } ${bold ? 'font-bold' : 'font-normal'
+      } font-['Roboto'] leading-normal`}
+    >
+      {children}
+    </Tag>
+  )
+}
